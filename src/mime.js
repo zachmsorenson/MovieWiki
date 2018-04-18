751ccb9e5046f596b12ee44c926d8d38833417ad
jahianancyyang <31836465+jahianancyyang@users.noreply.github.com>
2018-04-18 17:38:44 -0500

    some node_modules that I had to update?

--- src/mime.js
+++ src/mime.js
@@ -7,16 +7,3 @@ var mime_types = {
         jpeg:   'image/jpeg',
         gif:    'image/gif'
 };
-
-function hello(){
-	console.log('hello');
-}
-
-module.exports = {};
-
-module.exports.mime_types = mime_types;
-module.exports.version = '0.0.0';
-module.exports.world = function(){
-	console.log('world');
-}
-module.exports.hello = hello;
