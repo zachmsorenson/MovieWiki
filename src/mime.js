c584d3867b38eabc25c7041b606cae560326cff9
jahianancyyang <31836465+jahianancyyang@users.noreply.github.com>
2018-04-26 16:29:52 -0500

    More styling

--- src/mime.js
+++
@@ -1,9 +0,0 @@
-var mime_types = {
-        html:   'text/html',
-        css:    'text/css',
-        js:     'application/javascript',
-        png:    'image/png',
-        jpg:    'image/jpg',
-        jpeg:   'image/jpeg',
-        gif:    'image/gif'
-};
