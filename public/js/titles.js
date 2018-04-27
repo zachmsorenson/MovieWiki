54b5d22dd0137d0631333e90e3e2903941e44b16
Zach <sore1324@stthomas.edu>
2018-04-26 19:36:08 -0500

    Working on B section

--- public/js/titles.js
+++ public/js/titles.js
@@ -1,1 +1,15 @@
-console.log('hello');
+
+
+function toggleEditOverlay(){
+    var overlay = $('#edit_overlay');
+    console.log('toggle');
+    console.log(overlay);
+    console.log(overlay.css('display'));
+    if (overlay.css('display') == 'none'){
+        overlay.css('display', 'block');
+    } else {
+        overlay.css('display', 'none');
+    }
+}
+
+function
